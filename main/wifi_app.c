b29c38025474b11d5e5b87ff30d5fb1166e24f3d
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-05-12 20:11:11 +0700

    Use event handler

--- main/wifi_app.c
+++ main/wifi_app.c
@@ -15,23 +15,81 @@
 #include "lwip/netdb.h"
 
 #include "rgb_led.h"
+#include "tasks_common.h"
 #include "wifi_app.h"
 
 // Tag used for ESP serial console messages
 static const char TAG [] = "wifi_app";
 
+// Queue handle used to manipulate the main queue of events
+static QueueHandle_t wifi_app_queue_handle;
+
+/**
+ * Main task for the WiFi application
+ * @param pvParameters parameter which can be passed to the task
+ */
+static void wifi_app_task(void *pvParameters)
+{
+	wifi_app_queue_message_t msg;
+
+	// Send first event message
+	wifi_app_send_message(WIFI_APP_MSG_START_HTTP_SERVER);
+
+    for(;;) {
+		if (xQueueReceive(wifi_app_queue_handle, &msg, portMAX_DELAY))
+		{
+			switch (msg.msgID)
+			{
+				case WIFI_APP_MSG_LOAD_SAVED_CREDENTIALS:
+					ESP_LOGI(TAG, "WIFI_APP_MSG_LOAD_SAVED_CREDENTIALS");
+
+					break;
+
+				case WIFI_APP_MSG_START_HTTP_SERVER:
+					ESP_LOGI(TAG, "WIFI_APP_MSG_START_HTTP_SERVER");
+
+					break;
+
+				case WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER:
+					ESP_LOGI(TAG, "WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER");
+
+					break;
+
+				case WIFI_APP_MSG_STA_CONNECTED_GOT_IP:
+					ESP_LOGI(TAG, "WIFI_APP_MSG_STA_CONNECTED_GOT_IP");
+
+					break;
+
+				case WIFI_APP_MSG_USER_REQUESTED_STA_DISCONNECT:
+					ESP_LOGI(TAG, "WIFI_APP_MSG_USER_REQUESTED_STA_DISCONNECT");
+
+					break;
+
+				case WIFI_APP_MSG_STA_DISCONNECTED:
+					ESP_LOGI(TAG, "WIFI_APP_MSG_STA_DISCONNECTED");
+
+					break;
+
+				default:
+					break;
+
+			}
+		}
+    }
+}
+
 void wifi_app_start(void)
 {
-    while (true) {
-		ESP_LOGI(TAG, "WIFI_APP...");
+	// Create message queue
+	wifi_app_queue_handle = xQueueCreate(3, sizeof(wifi_app_queue_message_t));
 
-    	rgb_led_wifi_app_started();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+	// Start the WiFi application task
+	xTaskCreatePinnedToCore(&wifi_app_task, "wifi_app_task", WIFI_APP_TASK_STACK_SIZE, NULL, WIFI_APP_TASK_PRIORITY, NULL, WIFI_APP_TASK_CORE_ID);
+}
 
-    	rgb_led_http_server_started();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    	rgb_led_wifi_connected();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+BaseType_t wifi_app_send_message(wifi_app_message_e msgID)
+{
+	wifi_app_queue_message_t msg;
+	msg.msgID = msgID;
+	return xQueueSend(wifi_app_queue_handle, &msg, portMAX_DELAY);
 }
--- main/wifi_app.h
+++ main/wifi_app.h
@@ -8,6 +8,41 @@
 #ifndef MAIN_WIFI_APP_H_
 #define MAIN_WIFI_APP_H_
 
+#include "esp_netif.h"
+#include "esp_wifi_types.h"
+#include "freertos/FreeRTOS.h"
+
+/**
+ * Message IDs for the WiFi application task
+ * @note Expand this based on your application requirements.
+ */
+typedef enum wifi_app_message
+{
+	WIFI_APP_MSG_START_HTTP_SERVER = 0,
+	WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER,
+	WIFI_APP_MSG_STA_CONNECTED_GOT_IP,
+	WIFI_APP_MSG_USER_REQUESTED_STA_DISCONNECT,
+	WIFI_APP_MSG_LOAD_SAVED_CREDENTIALS,
+	WIFI_APP_MSG_STA_DISCONNECTED,
+} wifi_app_message_e;
+
+/**
+ * Structure for the message queue
+ * @note Expand this based on application requirements e.g. add another type and parameter as required
+ */
+typedef struct wifi_app_queue_message
+{
+	wifi_app_message_e msgID;
+} wifi_app_queue_message_t;
+
+/**
+ * Sends a message to the queue
+ * @param msgID message ID from the wifi_app_message_e enum.
+ * @return pdTRUE if an item was successfully sent to the queue, otherwise pdFALSE.
+ * @note Expand the parameter list based on your requirements e.g. how you've expanded the wifi_app_queue_message_t.
+ */
+BaseType_t wifi_app_send_message(wifi_app_message_e msgID);
+
 /**
  * Starts the WiFi RTOS task
  */
