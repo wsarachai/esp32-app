5c355a0bcbe1704575ecf71b83303a83bf77e722
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-05-12 20:24:03 +0700

    Startup WiFi

--- main/wifi_app.h
+++ main/wifi_app.h
@@ -12,6 +12,26 @@
 #include "esp_wifi_types.h"
 #include "freertos/FreeRTOS.h"
 
+// WiFi application settings
+#define WIFI_AP_SSID				"ESP32_AP"			// AP name
+#define WIFI_AP_PASSWORD			"password"			// AP password
+#define WIFI_AP_CHANNEL				1					// AP channel
+#define WIFI_AP_SSID_HIDDEN			0					// AP visibility
+#define WIFI_AP_MAX_CONNECTIONS		5					// AP max clients
+#define WIFI_AP_BEACON_INTERVAL		100					// AP beacon: 100 milliseconds recommended
+#define WIFI_AP_IP					"192.168.0.1"		// AP default IP
+#define WIFI_AP_GATEWAY				"192.168.0.1"		// AP default Gateway (should be the same as the IP)
+#define WIFI_AP_NETMASK				"255.255.255.0"		// AP netmask
+#define WIFI_AP_BANDWIDTH			WIFI_BW_HT20		// AP bandwidth 20 MHz (40 MHz is the other option)
+#define WIFI_STA_POWER_SAVE			WIFI_PS_NONE		// Power save not used
+#define MAX_SSID_LENGTH				32					// IEEE standard maximum
+#define MAX_PASSWORD_LENGTH			64					// IEEE standard maximum
+#define MAX_CONNECTION_RETRIES		5					// Retry number on disconnect
+
+// netif object for the Station and Access Point
+extern esp_netif_t* esp_netif_sta;
+extern esp_netif_t* esp_netif_ap;
+
 /**
  * Message IDs for the WiFi application task
  * @note Expand this based on your application requirements.
@@ -47,5 +67,10 @@ BaseType_t wifi_app_send_message(wifi_app_message_e msgID);
  * Starts the WiFi RTOS task
  */
 void wifi_app_start(void);
+
+/**
+ * Gets the wifi configuration
+ */
+wifi_config_t* wifi_app_get_wifi_config(void);
 
 #endif /* MAIN_WIFI_APP_H_ */
