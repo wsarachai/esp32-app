4f1fa148a53a556175b1860e5fd445f292f1095b
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-05-12 19:26:22 +0700

    LED Test

--- main/main.c
+++ main/main.c
@@ -2,10 +2,18 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+#include "rgb_led.h"
+
 void app_main(void)
 {
     while (true) {
-        printf("Hello from app_main!\n");
+    	rgb_led_wifi_app_started();
+        sleep(1);
+
+    	rgb_led_http_server_started();
+        sleep(1);
+
+    	rgb_led_wifi_connected();
         sleep(1);
     }
 }
