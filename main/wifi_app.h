da6bea087a307d800565edf1c0e42e6b386788fe
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-06-04 17:24:38 +0700

    Rename WiFi

--- main/wifi_app.h
+++ main/wifi_app.h
@@ -16,7 +16,7 @@
 typedef void (*wifi_connected_event_callback_t)(void);
 
 // WiFi application settings
-#define WIFI_AP_SSID				"WATER_CTL_AP"		// AP name
+#define WIFI_AP_SSID				"MJU-SmartFarm-V1.0"		// AP name
 #define WIFI_AP_PASSWORD			"password"			// AP password
 #define WIFI_AP_CHANNEL				1					// AP channel
 #define WIFI_AP_SSID_HIDDEN			0					// AP visibility
